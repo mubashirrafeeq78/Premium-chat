b2feedde7964379b0b970ed9bd8eb660f01d8c3b
mubashirrafeeq78 <officialmubashirhassan@gmail.com>
2025-12-23 08:32:52 +0500

    Update config.dart

--- lib/config.dart
+++ lib/config.dart
@@ -1,8 +1,4 @@
 class AppConfig {
-  // 🔹 صرف یہی URL بدلیں گے جب بھی سرور چینج ہو
-  static const String baseUrl =
-      "https://premiumchatbackend-production.up.railway.app";
-
-  // 🔹 API timeout (slow internet کیلئے مناسب)
+  static const String baseUrl = "http://127.0.0.1:8080";
   static const Duration timeout = Duration(seconds: 20);
 }
