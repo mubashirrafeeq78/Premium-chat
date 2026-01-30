1faa93703b5925e4a6bfe37d9264f0f71b1eda90
mubashirrafeeq78 <officialmubashirhassan@gmail.com>
2026-01-30 07:59:21 +0500

    Delete lib directory

--- lib/config.dart
+++
@@ -1,6 +0,0 @@
-class AppConfig {
-  static const String baseUrl =
-      "https://premiumchatbackend-1--bhaimubashir61.replit.app";
-
-  static const Duration timeout = Duration(seconds: 20);
-}
--- lib/utils.dart
+++
@@ -1,6 +0,0 @@
-import 'package:flutter/material.dart';
-
-void showSnack(BuildContext context, String msg) {
-  if (!context.mounted) return;
-  ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text(msg)));
-}
