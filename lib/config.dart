640bdbeb3fa3e5c4f0ceb816643903ae511a1247
mubashirrafeeq78 <officialmubashirhassan@gmail.com>
2026-04-06 07:19:35 +0500

    Add AppConfig class with base URL and API key

---
+++ lib/config.dart
@@ -0,0 +1,11 @@
+class AppConfig {
+  // اپنا سرور یو آر ایل یہاں لکھیں (آخر میں سلیش نہ لگائیں)
+  static const String baseUrl = "https://paxochat.com"; 
+
+  // آپ کی مخصوص سیکیورٹی کی
+  static const String apiKey = "PixoChat_Master_Secure_2026";
+
+  // اینڈ پوائنٹس (بیک اینڈ کی فائلوں کے نام)
+  static const String auth = "/auth";
+  static const String verifyOtp = "/verify-otp";
+}
